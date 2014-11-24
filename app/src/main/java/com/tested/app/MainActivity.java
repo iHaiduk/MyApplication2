7bb7a868cabfe1d5c3e0cd4c665110226c15d9cc
Ihor Gayduk <htgroup.dev@gmail.com>
2014-11-25 00:43:56 +0200

    err

--- app/src/main/java/com/tested/app/MainActivity.java
+++ app/src/main/java/com/tested/app/MainActivity.java
@@ -2,38 +2,20 @@ package com.tested.app;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import com.tested.util.DBHelper;
 import com.tested.util.RequestCode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-
 public class MainActivity extends Activity {
-
-    private DBHelper mydb ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
-
-
 
     public void onShow(View v){
         Intent intent;
-        mydb = new DBHelper(this);
-        Log.i("DEV", mydb.getDatabaseName());
         switch (v.getId()){
             case R.id.admin:
                 intent = new Intent(this, AdminActivity.class);
